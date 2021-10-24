a0bbc547ba8ba2eac01c3d9b58df36d1be94fb09
Luccas Rafael <luccasrm@gmail.com>
2021-10-24 10:39:03 -0300

    Refactor using PageObjects

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -1,12 +1,11 @@
 package modulos.produto;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import telas.LoginTela;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -14,9 +13,10 @@ import java.util.concurrent.TimeUnit;
 
 @DisplayName("Testes Mobile do Modulo de Produto")
 public class ProdutoTest {
-    @DisplayName("Validacao do valor de Produto Nao permitido")
-    @Test
-    public void testValidacaoDoValorDeProdutoNaoPermitido() throws MalformedURLException {
+    private WebDriver app;
+
+    @BeforeEach
+    public void beforeEach() throws MalformedURLException {
         // Abrir o app
 
         DesiredCapabilities capacidades = new DesiredCapabilities();
@@ -27,37 +27,32 @@ public class ProdutoTest {
         capacidades.setCapability("appActivity","com.lojinha.ui.MainActivity");
         capacidades.setCapability("app", "C:\\Users\\lucca\\Downloads\\lojinha-nativa.apk");
 
-        WebDriver app = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capacidades);
+        this.app = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capacidades);
         app.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-        // Fazer Login
-        app.findElement(By.id("com.lojinha:id/user")).click();
-        app.findElement(By.id("com.lojinha:id/user")).findElement(By.id("com.lojinha:id/editText")).sendKeys("luccasrafael");
+    }
+    @DisplayName("Validacao do valor de Produto Nao permitido")
+    @Test
+    public void testValidacaoDoValorDeProdutoNaoPermitido() {
 
-        app.findElement(By.id("com.lojinha:id/password")).click();
-        app.findElement(By.id("com.lojinha:id/password")).findElement(By.id("com.lojinha:id/editText")).sendKeys("123456");
-
-        app.findElement(By.id("com.lojinha:id/loginButton")).click();
-
-        // Abrir o formulario de novo produto
-
-        app.findElement(By.id("com.lojinha:id/floatingActionButton")).click();
-
-        // Cadastrar um produto com valor invalido
-        app.findElement(By.id("com.lojinha:id/productName")).click();
-        app.findElement(By.id("com.lojinha:id/productName")).findElement(By.id("com.lojinha:id/editText")).sendKeys("Iphone");
-
-        app.findElement(By.id("com.lojinha:id/productValue")).click();
-        app.findElement(By.id("com.lojinha:id/productValue")).findElement(By.id("com.lojinha:id/editText")).sendKeys("700001");
-
-        app.findElement(By.id("com.lojinha:id/productColors")).click();
-        app.findElement(By.id("com.lojinha:id/productColors")).findElement(By.id("com.lojinha:id/editText")).sendKeys("preto, cinza");
-
-        app.findElement(By.id("com.lojinha:id/saveButton")).click();
+        String mensagemApresentada = new LoginTela(app)
+                .preencherUsuario("luccasrafael")
+                .preencherSenha("123456")
+                .submeterLogin()
+                .abrirTelaAdicaoProduto()
+                .preencherNomeProduto("Iphone")
+                .preencherValorProduto("700001")
+                .preencherCoresProduto("Preto, Branco")
+                .submissaoComErro()
+                .obterMensagemDeErro();
 
         // Validar que a mensagem de valor invalido foi apresentada
-        String mensagemApresentada = app.findElement(By.xpath("//android.widget.Toast")).getText();
         Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemApresentada);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        app.quit();
     }
 
 }
